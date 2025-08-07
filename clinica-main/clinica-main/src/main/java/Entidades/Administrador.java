d1049c73c44552e9fcd5ba709762a75553712d5c
raissa7r <raissarodrigues0716@gmail.com>
2025-08-07 20:35:27 -0300

    tela CMédico

--- clinica-main/clinica-main/src/main/java/Entidades/Administrador.java
+++ clinica-main/clinica-main/src/main/java/Entidades/Administrador.java
@@ -13,7 +13,7 @@ public class Administrador {
     private int id;
     private String nome;
     private String login;
-    private String senha;
+    private String Senha;
     private String senha;
 
     /**
